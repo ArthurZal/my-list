62b8f9bd38266390f733d9f5174dfedd8a14d338
Arthur Zall <zallarthur@gmail.com>
2024-08-16 23:09:29 -0300

    Estruturação da função para criar um novo elemento para cada novo item adicionado à lista de compras

--- scripts.js
+++ scripts.js
@@ -1,21 +1,50 @@
-const formAddItems = document.querySelector("#addItems")
+const body = document.querySelector("body");
+const formAddItems = document.querySelector("#addItems");
 const input = document.querySelector("#newItem");
-
+const paragraph = document.querySelector("p");
+const message = document.querySelector("#msg-delete");
 
 
 formAddItems.onsubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     if(input.value == 0) {
-        alert("Please, enter a valid item")
+        alert("Please, enter a valid item");
 
     } else {
-        addNewItem(input.value)
+        addNewItem();
+        input.value = "";
     }
 
-
 }
 
-function addNewItem(item) {
-    console.log(item)
-}
+function addNewItem() {
+ 
+    paragraph.classList.add("disable");
+
+    createNewViewElements(input.value);
+    
+};
+
+function createNewViewElements(item) {
+    const newForm = document.createElement("form");
+    const newCheckbox = document.createElement("Input");
+    const newLabel = document.createElement("Label");
+    const newParagraph = document.createElement("p");
+    const newButton = document.createElement("button");
+
+    newCheckbox.type = "checkbox";
+    newButton.textContent = "DELETE";
+
+    newButton.onclick = () => {
+        newForm.remove();
+        
+    };
+ 
+    newLabel.append(newCheckbox, newParagraph,newButton);
+    newForm.append(newLabel);
+    body.append(newForm);
+
+    newParagraph.innerText = item;
+
+};
